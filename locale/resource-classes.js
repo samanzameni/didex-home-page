a68dc5f8f41ea1b925f1b182163d234ecc97dc44
Azad Kavian <azad.kavian@gmail.com>
2020-08-31 03:04:25 +0430

    added locale service

---
+++ locale/resource-classes.js
@@ -0,0 +1,22 @@
+const RESOURCE = {
+  en: {
+    navbar_signup: "Sign up",
+    navbar_signin: "Sign in",
+    navbar_home: "Home",
+    navbar_trade: "Trade",
+    navbar_invest: "Invest",
+    header_title: "Trade",
+    header_subtitle: "Start your crypto journey!",
+    header_cta: "Get started",
+  },
+  fa: {
+    navbar_signup: "ثبت نام",
+    navbar_signin: "ورود",
+    navbar_home: "خانه",
+    navbar_trade: "بازار حرفه‌ای",
+    navbar_invest: "سرمایه‌گذاری",
+    header_title: "پلتفرم معاملات ارز دیجیتال",
+    header_subtitle: "تجربه‌ای امن از تجارت جهانی!",
+    header_cta: "شروع کنید",
+  },
+};
---
+++ scripts/locale.js
@@ -0,0 +1,45 @@
+let currentLocale;
+
+function initLocale() {
+  let tempLocale;
+  if (localStorage.getItem("didexLocale")) {
+    tempLocale = localStorage.getItem("didexLocale");
+  } else {
+    tempLocale = window.location.hostname.endsWith(".ir") ? "fa" : "en";
+  }
+
+  changeLocale(tempLocale);
+}
+
+function changeLocale(newLocale, shouldStore = false) {
+  currentLocale = newLocale;
+  if (shouldStore) {
+    localStorage.setItem("didexLocale", currentLocale);
+  }
+
+  updateTextContent();
+  updateBodyClass();
+}
+
+function updateTextContent() {
+  const elements = document.querySelectorAll(".ddx-locale");
+
+  elements.forEach(function (el) {
+    el.innerHTML = RESOURCE[currentLocale][el.dataset.resource_code] || "";
+  });
+}
+
+function updateBodyClass() {
+  const html = document.getElementsByTagName("html").item(0);
+  html.setAttribute("lang", currentLocale);
+
+  const body = document.getElementsByTagName("body").item(0);
+
+  ["fa", "en"].forEach(function (l) {
+    body.classList.remove(l);
+  });
+
+  body.classList.add(currentLocale);
+}
+
+initLocale();

649d85a23de18f01ecb47b4b557d71df0956a384
Azad Kavian <azad.kavian@gmail.com>
2020-08-31 11:16:03 +0430

    added iransans fonts

--- locale/resource-classes.js
+++ locale/resource-classes.js
@@ -88,10 +88,10 @@ const RESOURCE = {
     footer_links_app: "Application",
     footer_links_rules: "Trading Rules",
     footer_links_product: "Product",
-    footer_links_trade: "Trade",
-    footer_links_invest: "Invest",
     footer_links_exchange: "Exchange",
     footer_links_p2p: "P2P Exchange",
+    footer_links_trade: "Trade",
+    footer_links_Invest: "Invest",
     footer_links_pay: "DidPay",
     footer_links_support: "Support",
     footer_links_faq: "FAQ",
@@ -109,5 +109,95 @@ const RESOURCE = {
     header_title: "پلتفرم معاملات ارز دیجیتال",
     header_subtitle: "تجربه‌ای امن از تجارت جهانی!",
     header_cta: "شروع کنید",
+
+    navbar_second_Trade: "بازار حرفه‌ای",
+    navbar_second_Chart: "نمودار",
+    navbar_second_Invest: "سرمایه‌گذاری",
+    navbar_second_App: "اپلیکیشن",
+    navbar_second_Exchange: "خرید ‌و‌ فروش مستقیم",
+    navbar_second_Features: "ویژگی‌ها",
+    navbar_second_Pay: "درگاه پرداخت",
+    navbar_second_Wallet: "کیف پول",
+
+    charts_volume_7d: "حجم معاملات هفت‌روزه",
+    charts_volume_24h: "حجم معاملات 24ساعته",
+    charts_volume_users: "کاربران",
+
+    charts_title_world: "قیمت جهانی",
+    charts_title_live: "قیمت لحظه‌ای",
+
+    charts_live_Volume: "حجم معاملات",
+    charts_live_Change: "درصد تغییرات",
+    charts_live_Chart: "نمودار",
+    charts_live_buy: "خرید",
+
+    invest_title: "سرمایه‌گذاری",
+    invest_subtitle: "مکان سرمایه‌گذاری را بشناسید تا سود مطلوبتان را به‌دست آورید",
+    invest_cta: "شروع کنید",
+
+    app_coming_soon: "...به‌زودی",
+    app_title: "اپلیکیشن",
+    app_subtitle: "بیش از 45 رمز‌ارز متنوع را با قیمت واقعی، بدون کارمزد و بهره از ما بخواهید",
+    app_cta: "اطلاعات بیشتر",
+
+    exchange_coming_soon: "...به‌زودی",
+    exchange_title: "خرید و فروش مستقیم",
+    exchange_subtitle:
+      "ارائه‌ای قدرتمند از دیدکس، با نقدینگی بالا، کارمزد پایین و بهترین‌قیمت رقابتی",
+    exchange_cta: "اطلاعات بیشتر",
+
+    features_title: "ویژگی‌ها",
+    features_subtitle:
+      "امکانات پیشرفته و منحصربه‌فرد، خرید و فروش بیت‌کوین و سایر رمزارزها با تجربه‌ای متفاوت",
+    features_card_title_support: "با هر قدم در کنارتان هستیم!",
+    features_card_body_support:
+      "با پشتیانی حرفه ای 24/7 همراه با آخرین اخبار و نکات دنیای ارزهای دیجیتال با دیدکس به روز باشید!",
+    features_card_title_best: "ما بهترین‌ها را فراهم کرده‌ایم",
+    features_card_body_best:
+      "تیمی حرفه‌ای با دانش روز در حوزه بلاکچین، مالی، دارایی‌های دیجیتال و هوش مصنوعی",
+    features_card_title_secure: "ثبات و امنیت",
+    features_card_body_secure:
+      "سیستم تامین امنیت اختصاصی چندلایه و غیرقابل نفوذ دیدکس، آماده محافظت از رمزارزهای شماست!",
+    features_card_title_ease: "تجربه خرید آسان رمزارز",
+    features_card_body_ease:
+      "!رمزارز مورد نظر خود را با استفاده از کارت اعتباری خریداری کنید و از بهترین نرخ‌های موجود در بازار لذت ببرید",
+
+    pay_coming_soon: "...به‌زودی",
+    pay_title: "درگاه پرداخت",
+    pay_subtitle: "پرداخت و دریافت کریپتو در هر مکان و هر زمان",
+    pay_cta: "اطلاعات بیشتر",
+
+    wallet_coming_soon: "...به‌زودی",
+    wallet_title: "کیف پول",
+    wallet_subtitle: "مطابق با پیشرفته‌ترین روش‌های تامین امنیت",
+    wallet_cta: "اطلاعات بیشتر",
+
+    footer_subscription_body:
+      "با عضویت در خبرنامه ما از آخرین اخبار روز دنیای ارزهای دیجیتال و پیشنهادات ویژه دیدکس مطلع باشید",
+    footer_subscription_submit: "عضویت",
+
+    footer_links_company: "شرکت",
+    footer_links_about: "درباره ما",
+    footer_links_careers: "فرصتهای شغلی",
+    footer_links_terms: "شرایط استفاده",
+    footer_links_privacy: "حریم خصوصی",
+    footer_links_contact: "ارتباط با ما",
+    footer_links_services: "خدمات",
+    footer_links_api: "API",
+    footer_links_fees: "کارمزدها و محدودیت ها",
+    footer_links_app: "اپلیکیشن",
+    footer_links_rules: "مقررات معامله",
+    footer_links_product: "محصولات",
+    footer_links_exchange: "خرید و فروش مستقیم",
+    footer_links_p2p: "خرید و فروش همتا به همتا",
+    footer_links_trade: "بازار حرفه ای",
+    footer_links_Invest: "سرمایه گذاری",
+    footer_links_pay: "درگاه پرداخت",
+    footer_links_support: "پشتیبانی",
+    footer_links_faq: "سوالات متداول",
+    footer_links_help: "راهنما",
+    footer_links_security: "ملاحظات امنیتی",
+    footer_links_apidoc: "مستندات API",
+    footer_links_glossary: "واژه نامه",
   },
 };

813f0aae049043c300fd11c229023213b38b908b
Azad Kavian <azad.kavian@gmail.com>
2020-09-03 11:50:23 +0430

    added locale selector

--- scripts/locale.js
+++ scripts/locale.js
@@ -17,6 +17,8 @@ function changeLocale(newLocale, shouldStore = false) {
     localStorage.setItem("didexLocale", currentLocale);
   }
 
+  AJAX_DATA_DICT.currentLocale = currentLocale;
+  updateAJAXLoaders();
   updateTextContent();
   updateBodyClass();
   updateAssets();

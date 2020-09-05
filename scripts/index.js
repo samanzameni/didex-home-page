9c6adc021945504d4595e789411efe3ef54c1de9
Azad Kavian <azad.kavian@gmail.com>
2020-09-05 18:25:18 +0430

    fixed TYPO :D

--- scripts/index.js
+++ scripts/index.js
@@ -49,8 +49,9 @@ function updateAJAXLoaders() {
 
   elements.forEach(function (el) {
     if (el.dataset.format === "currency") {
-      el.innerHTML =
-        AJAX_DATA_DICT[el.dataset.ajax]?.toLocaleString() + el.dataset.currency;
+      el.innerHTML = AJAX_DATA_DICT[el.dataset.ajax]
+        ? AJAX_DATA_DICT[el.dataset.ajax].toLocaleString() + el.dataset.currency
+        : "";
     } else if (el.dataset.format === "number") {
       el.innerHTML = AJAX_DATA_DICT[el.dataset.ajax]
         ? AJAX_DATA_DICT[el.dataset.ajax].toLocaleString()
